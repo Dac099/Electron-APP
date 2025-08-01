cf3e8b18f18d8a00f7c080673d605de403ffe790
David Ceja Zapata <d.ceja@ynemx.com>
2025-08-01 17:06:37 -0600

    Initial Code

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'My Electron App',
+    width: 800,
+    height: 600,
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file',
+  })
+
+  mainWindow.loadURL(startUrl);
+}
+
+app.whenReady().then(createMainWindow);
